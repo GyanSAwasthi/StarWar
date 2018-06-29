596e260a122defc656c3d8b2a3dd2f06fc156da3
Himansh <himanshu.hansinfotech@gmail.com>
2018-06-29 19:28:39 +0530

    Final Commit

--- app/src/main/java/in/penzebra/gyan/com/starwar/MainActivity.java
+++ app/src/main/java/in/penzebra/gyan/com/starwar/MainActivity.java
@@ -1,13 +1,40 @@
-package in.penzebra.gyan.com.starwar;
-
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-}
+//package in.penzebra.gyan.com.starwar;
+//
+//import android.support.v7.app.AppCompatActivity;
+//import android.os.Bundle;
+//
+//public class MainActivity extends AppCompatActivity implements IUserList{
+//    private static ListPresenter lPresenter;
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        if (lPresenter == null) {//first time creating activity = create presenter
+//            lPresenter = new ListPresenter();
+//        }
+//    }
+//
+//
+//    @Override
+//    public void onSuccess() {
+//
+//    }
+//
+//    @Override
+//    public void onError(String error) {
+//
+//    }
+//
+//    @Override
+//    public void showProgress(boolean show) {
+//
+//    }
+//
+//    @Override
+//    protected void onDestroy() {
+//        super.onDestroy();
+//        if (isFinishing()) {//activity finishing, release reference to presenter
+//            lPresenter = null;
+//        }
+//    }
+//}
